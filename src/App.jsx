49f40d5857fd484027a812bbb8190bccc2179719
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-02-03 22:35:32 +0500

    still working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Router from './Configuration/Router';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router />
     </div>
   );
 }
---
+++ src/Configuration/Router.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Navbar from '../Components/Navbar'
+import Login from '../Pages/Login';
+
+export default function Router() {
+    const [isUser, setIsUser] = useState(false);
+
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={
+                        isUser ? <Navbar><Home /></Navbar> :
+                            <Navigate to={"/login"} />
+                    } />
+                    <Route path='/login' element={
+                        isUser ? <Navigate to={"/"} /> :
+                            <Login />
+                    } />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
