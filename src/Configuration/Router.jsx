66804c4241e463797077e7db8f6edf86d1eacbb2
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-02-04 14:42:30 +0500

    login component done

--- src/Configuration/Router.jsx
+++ src/Configuration/Router.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Navbar from '../Components/Navbar'
-import Login from '../Pages/Login';
+import Login from '../Pages/Login/Login'
 
 export default function Router() {
     const [isUser, setIsUser] = useState(false);
